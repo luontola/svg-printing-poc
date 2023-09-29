d6cb0a0eaf32701fb2357010895dea747a11fd82
Esko Luontola <esko@luontola.fi>
2023-09-29 14:43:07 +0200

    Convert the static HTML page to a template

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
+import base64
 import http.server
 import socketserver
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 PORT = 8080
-TEMPLATE_DIR = "templates"
+TEMPLATE_DIR = "."
 
 # Create a Jinja2 environment for rendering templates
 jinja2_env = Environment(
@@ -13,7 +14,6 @@ jinja2_env = Environment(
 )
 
 
-# Create a custom request handler to render HTML templates
 class TemplateHandler(http.server.SimpleHTTPRequestHandler):
     def render_template(self, template_name, context=None):
         if context is None:
@@ -26,21 +26,26 @@ class TemplateHandler(http.server.SimpleHTTPRequestHandler):
         if "." in self.path:
             super().do_GET()
         else:
-            # Render the HTML template
+            with open('image.svg', 'r') as file:
+                image_svg = file.read()
+            with open('5cm.svg', 'r') as file:
+                image_5cm_svg = file.read()
+
             self.send_response(200)
-            self.send_header('Content-type', 'text/html; charset=UTF-8')
+            self.send_header('Content-type', 'text/html; charset=utf-8')
             self.end_headers()
             context = {
-                'message': 'Hello from <u>Python</u>!',
+                'inline_svg': image_svg.replace('<svg', '<svg id="inline"', 1),
+                'base64_svg': base64.b64encode(image_svg.encode('utf-8')).decode('utf-8'),
+                'inline_5cm_svg': image_5cm_svg,
             }
-            html = self.render_template("index.html", context)
-            self.wfile.write(html.encode('UTF-8'))
+            html = self.render_template("template.html", context)
+            self.wfile.write(html.encode('utf-8'))
 
 
 # Avoid "OSError: [Errno 48] Address already in use" when restarting the server
 socketserver.TCPServer.allow_reuse_address = True
 
-# Create and configure the server
 with socketserver.TCPServer(("localhost", PORT), TemplateHandler) as httpd:
     print(f"Serving at http://localhost:{PORT}")
     try:
